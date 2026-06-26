6c768a06bdd764fe7321154a19b2972c98bd4ce1
eduardo2010-wash <eduardo.bacciotti.ribeiro@escola.pr.gov.br>
2026-06-26 11:34:42 -0300

    Add Clientes and Compras tables to database
    Create tables for Clients and Purchases with relationships.

---
+++ sistema-livraria2.sql
@@ -0,0 +1,28 @@
+-- Criação da tabela Clientes
+CREATE TABLE Clientes (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomeCliente TEXT NOT NULL,
+    emailCliente TEXT UNIQUE
+);
+
+-- Criação da tabela Compras
+CREATE TABLE Compras (
+    CompraID INTEGER PRIMARY KEY AUTOINCREMENT,
+    ClienteID INTEGER NOT NULL,
+    NomeLivro TEXT NOT NULL,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
+);
+SELECT * FROM Clientes; 
+SELECT * FROM Clientes;
+SELECT * FROM Compras;
+SELECT
+    Clientes.nomeCliente,
+    Compras.NomeLivro
+FROM Clientes
+INNER JOIN Compras
+ON Clientes.ID = Compras.ClienteID;
+SELECT
+    Clientes.nomeCliente,
+    Compras.NomeLivro
+FROM Clientes
+LEFT JOIN Compras ON Clientes.ID = Compras.ClienteID;
