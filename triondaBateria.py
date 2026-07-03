ca9b8f9637aae8a243e903445c4ebd9886085a98
eduardo2010-wash <eduardo.bacciotti.ribeiro@escola.pr.gov.br>
2026-07-03 08:16:23 -0300

    Add battery monitoring for Trionda ball

---
+++ triondaBateria.py
@@ -0,0 +1,11 @@
+# Monitoramento da bateria da bola Trionda
+
+bateria_atual = 50
+bola_em_jogo = True
+
+if bateria_atual < 15 and bola_em_jogo:
+    print("ALERTA MÁXIMO: Bateria baixa! Substitua a bola na próxima paralisação.")
+elif bateria_atual < 15 and not bola_em_jogo:
+    print("Aviso: Bateria baixa. Aproveite a bola parada para trocá-la.")
+else:
+    print("Sistema Trionda operando normalmente. Bateria ok.")
